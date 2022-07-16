f364e4746ddc7856cb6fbe45f3992225122c1891
Richard Hillsdon <richard.hillsdon@rogers.com>
2022-07-16 16:39:01 -0400

    Event Work
    Worked on some event stuff.

--- Assets/Scripts/EventDB.cs
+++ Assets/Scripts/EventDB.cs
@@ -2,17 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventDB : MonoBehaviour
+public enum EventScaleEnum { slot, node, global };
+
+public abstract class Event
 {
-    // Start is called before the first frame update
-    void Start()
+    protected int meventId;
+    protected EventScaleEnum meventScale;
+    protected float mweight;
+
+    public int getID()
     {
-        
+        return meventId;
     }
 
-    // Update is called once per frame
-    void Update()
+    public EventScaleEnum getScale()
     {
-        
+        return meventScale;
     }
+
+    public float getWeight()
+    {
+        return mweight;
+    }
+
+    public abstract void FireEvent();
+}
+
+public abstract class EventList
+{
+    protected int mlistId;
+    protected int msafeWeight; //The chance that nothing happens.
+    protected List<Event> eventsList = new()
+    {
+
+    };
+
+    public EventList(int listid, int safeweight)
+    {
+        mlistId = listid;
+        msafeWeight = safeweight;
+    }
+
+    public int getID()
+    {
+        return mlistId;
+    }
+
+    public Event RollEvents() //Rolls for an event on the EventList
+    {
+        float totalWeight = msafeWeight;
+        for (int i = 0; i < eventsList.Count; i++)
+        {
+            totalWeight += eventsList[i].getWeight();
+        }
+        //Debug.Log(totalWeight);
+        float itemWeightIndex = Random.Range(0.0f, totalWeight);
+        float currentIndexWeight = 0.0f;
+        for (int i = 0; i < eventsList.Count; i++)
+        {
+            currentIndexWeight += (float)i;
+            if (currentIndexWeight >= itemWeightIndex)
+            {
+                return eventsList[i];
+            }
+        }
+
+
+        return null;
+
+    }
+}
+
+
+//MASTER EVENT LISTS DB PAST THIS POINT
+//DONT ADD ANYTHING ELSE
+
+//MASTER EVENT DB PAST THIS POINT
+//DONT ADD ANYTHING ELSE
+
+public class DefaultEvent : Event
+{
+    public DefaultEvent()
+    {
+        meventId = 0;
+        meventScale = (EventScaleEnum)3;
+        mweight = 0.0f;
+    }
+
+    public override void FireEvent()
+    {
+        Debug.Log("Default Event fired. Nothing should happen.");
+    }
+
 }
--- Assets/Scripts/EventHandler.cs
+++ Assets/Scripts/EventHandler.cs
@@ -2,22 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*ResourceDistribution
+EventHandler
+EventGetter
+RandomEventGetter
+GlobalEventFlag
+GlobalEventGetter*/
+
 public class EventHandler : MonoBehaviour
 {
+    public int mGlobalThreat;
+    public List<Event> globalEventsList = new()
+    {
+    };
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        globalEventsList.Add(new DefaultEvent());
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public void InitalizeEventList() //Initalizes the global list of events.
+    {
         
     }
 
-    void ResourceDistribution() //Distributes 
+    public void ResourceDistribution() //Applies a global resource modifier.
     {
 
     }
+
+    public void FireEvent(int targetEventID) //Fires a specific event by ID
+    {
+
+    }
+
+    public void EventRoll(int localThreat, int targetListID) //Rolls for a random event, from the specified event list.
+    {
+
+    }
+    
 }
