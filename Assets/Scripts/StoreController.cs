0f46f85fc82353194f3311c290c82ae4f3dbb8db
Conrad Belaire <cbelaire@gmail.com>
2022-07-16 20:11:09 -0400

    Started store

---
+++ Assets/Scripts/StoreController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreController : MonoBehaviour
+{
+    public List<StoreItem> items;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
